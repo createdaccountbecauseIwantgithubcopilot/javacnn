b19237b9439562b98ad5903bc2b925a86208db90
Your Name <you@example.com>
2025-05-27 14:36:29 -0400

    Revert "clearer names"
    This reverts commit 95d9a6b94c813e19a7b98f58aaa38a84f2db1a75.

--- com/neuralnetwork/loss.java
+++ com/neuralnetwork/loss.java
@@ -1,5 +1,17 @@
 package com.neuralnetwork;
-public abstract class loss {
-    public abstract float compute_loss(float[] predict, float[] ans);
-    public abstract float[] differentiate(float[] predict, float[] ans);
+public class loss {
+    public float compute_loss(float[] predict, float[] ans){
+        float sum = 0;
+        for (int x = 0; x < predict.length; x++){
+            sum += Math.abs(predict[x] - ans[x]);
+        }
+        return sum / predict.length;
+    }
+    public float[] differentiate(float[] predict, float[] ans){
+        float[] sum = new float[predict.length];
+        for (int x = 0; x < predict.length; x++){
+            sum[x] = 1 / predict.length;
+        }
+        return sum;
+    }
 }
