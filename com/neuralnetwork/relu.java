5f9cb7196df7d245e64c9665ca66c152f8df2667
Your Name <you@example.com>
2025-05-27 14:41:52 -0400

    small error

--- com/neuralnetwork/relu.java
+++ com/neuralnetwork/relu.java
@@ -1,13 +1,13 @@
 package com.neuralnetwork;
 class relu extends activation{
-    float activate(float input){
+    public float activate(float input){
         if (input > 0){
             return input;
         }else{
             return 0;
         }
     }
-    float differentiate(float input){
+    public float differentiate(float input){
         if (input > 0) return 1;
         else return 0;
     }
