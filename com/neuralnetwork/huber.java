df29b239d86ab35faffb18a44523b75d23d3e0ec
Your Name <you@example.com>
2025-05-28 09:17:00 -0400

    for some reasons neovim didn't save the files (I had vscode open).

--- com/neuralnetwork/huber.java
+++ com/neuralnetwork/huber.java
@@ -1,23 +1,38 @@
 package com.neuralnetwork;
+
 public class huber extends loss{
+
+    huber(float delta){
+      this.delta = delta;
+    }
+
     public float compute_loss(float[] predict, float[] ans){
         float sum = 0;
+        float delta = 1.0f;
         for (int x = 0; x < predict.length; x++){
-          if (predict[x] - ans[x] >= 1){
-            sum += (float)(Math.pow(predict[x] - ans[x], 2));
-          } else{
-            sum += (float)Math.abs(predict[x] - ans[x]);
-          }
+            float error = predict[x] - ans[x];
+            float absError = Math.abs(error);
+            if (absError <= delta) {
+                sum += 0.5f * Math.pow(error, 2);
+            } else {
+                sum += delta * (absError - (0.5f * delta));
+            }
         }
         return sum / predict.length;
     }
+
     public float[] differentiate(float[] predict, float[] ans){
         float[] diff = new float[predict.length];
+        float delta = 1.0f;
         for (int x = 0; x < predict.length; x++){
-          if (predict[x] - ans[x] >= 1)
-            diff[x] = 2.0f * (predict[x] - ans[x]) / predict.length;
-          else
-            diff[x] = Math.abs(predict[x] - ans[x]);
+            float error = predict[x] - ans[x];
+            float absError = Math.abs(error);
+            if (absError <= delta) {
+                diff[x] = error;
+            } else {
+                diff[x] = delta * Math.signum(error);
+            }
+            diff[x] /= predict.length;
         }
         return diff;
     }
